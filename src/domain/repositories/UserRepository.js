2641d9ee0ce781bef711631ed5659ba8a2fd389a
Jin garam <2001garam@naver.com>
2022-06-20 16:36:28 +0900

    #1 Feat: 의존성 관리를 위한 레포지토리(DAO) 추상화

---
+++ src/domain/repositories/UserRepository.js
@@ -0,0 +1,13 @@
+export default class UserRepository {
+  findByEmail(email) {
+    throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
+  }
+
+  findById(userId) {
+    throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
+  }
+
+  save(userEntity) {
+    throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
+  }
+}
