79dc68a1ca59d14b8c5722d22e9af55a8149c49d
Jin garam <2001garam@naver.com>
2022-07-05 13:18:28 +0900

    [Fix] productController 책임 분리, createProduct & getProducts (#21)

--- src/interface/controllers/productController.js
+++ src/interface/controllers/productController.js
@@ -2,234 +2,81 @@
 import asyncHandler from 'express-async-handler';
 import mongoose from 'mongoose';
 import Product from '../../infrastructure/database/mongoose/models/product.js';
-import Artist from '../../infrastructure/database/mongoose/models/artist.js';
 import isAuthorized from '../../utils/isAuthorized.js';
+import { HTTP_STATUS } from '../../infrastructure/config/constants.js';
 
 const { ObjectId } = mongoose.Types;
 
-// @desc   Create a product
-// @route  POST /api/products
-// @access Private/Admin
-const createProduct = asyncHandler(async (req, res) => {
-  const {
-    artist,
-    artCode,
-    title,
-    image,
-    theme,
-    price,
-    info: { details, size, canvas, createdAt },
-  } = req.body;
+export class ProductController {
+  #productService;
 
-  if (
-    artist &&
-    artCode &&
-    title &&
-    theme &&
-    price &&
-    image &&
-    details &&
-    size &&
-    canvas &&
-    createdAt
-  ) {
-    await Artist.updateOne({ _id: artist }, { $inc: { countOfWorks: 1 } });
-    const product = await Product.create({
-      artist,
-      artCode,
-      title,
-      image,
-      theme,
-      price,
-      info: { details, size, canvas, createdAt },
-    });
-    res.status(201).json(product);
-  } else {
-    res.status(400).json({ message: '상품 정보를 모두 입력해주세요' });
+  constructor(productService) {
+    this.#productService = productService;
   }
-});
 
-// @desc   Fetch all products
-// @route  GET /api/products
-// @access Public
-const getProducts = asyncHandler(async (req, res) => {
-  // 관리자 권한일 때와 분기 나눠 주기
-  const { pageNumber, isAdmin } = req.query;
-  const page = Number(pageNumber) || 1;
+  // @desc   Create a product
+  // @route  POST /api/products
+  // @access Private/Admin
+  createProduct = asyncHandler(async (req, res) => {
+    const data = await this.#productService.createProduct(req.body);
 
-  let pageSize;
-  let count;
-  let products;
+    res.status(HTTP_STATUS.CREATE).json(data);
+  });
 
-  if (isAdmin === 'true') {
-    req.user = isAuthorized(req);
-    if (req.user) {
-      // 토큰이 유효할 경우
-      if (req.user.isAdmin === true) {
-        // 관리자인 경우
-        pageSize = 10;
-        count = await Product.countDocuments({});
-        products = await Product.find({})
-          .populate('artist', ['name', 'aka', 'code', 'record'])
-          .sort({ _id: -1 })
-          .limit(pageSize)
-          .skip(pageSize * (page - 1))
-          .exec();
+  // @desc   Fetch all products
+  // @route  GET /api/products
+  // @access Public
+  getProducts = asyncHandler(async (req, res) => {
+    const { pageNumber, isAdmin, ...searchQuery } = req.query;
+    const page = Number(pageNumber) || 1;
+    let data;
 
-        res.json({ products, page, pages: Math.ceil(count / pageSize) });
-        return;
+    if (isAdmin === 'true') {
+      req.user = isAuthorized(req);
+      if (req.user?.isAdmin) {
+        // 관리자의 상품관리페이지
+        data = await this.#productService.getProducts(page, true);
+      } else {
+        throw Error('Not authorized, token failed');
       }
+    } else {
+      data = await this.#productService.getProducts(page, false, searchQuery);
     }
-  }
-
-  const keyword = req.query.keyword
-    ? {
-        $or: [
-          {
-            title: {
-              $regex: req.query.keyword,
-              $options: 'i',
-            },
-          },
-          {
-            'info.details': {
-              $regex: req.query.keyword,
-              $options: 'i',
-            },
-          },
-          {
-            'artist.name': {
-              $regex: req.query.keyword,
-              $options: 'i',
-            },
-          },
-          {
-            'artist.aka': {
-              $regex: req.query.keyword,
-              $options: 'i',
-            },
-          },
-        ],
-      }
-    : {};
-
-  const { theme, sizeMin, sizeMax, priceMin, priceMax } = req.query;
-
-  let filter = '';
-
-  if (theme) {
-    filter = { theme };
-  } else if (sizeMin && sizeMax) {
-    filter = {
-      'info.canvas': { $gte: Number(sizeMin), $lte: Number(sizeMax) },
-    };
-  } else if (priceMin && priceMax) {
-    filter = { price: { $gte: Number(priceMin), $lte: Number(priceMax) } };
-  } else if (priceMin) {
-    filter = { price: { $gte: Number(priceMin) } };
-  }
-
-  pageSize = 28;
-
-  products = await Product.aggregate([
-    { $match: { inStock: true } },
-    {
-      $lookup: {
-        from: 'artists',
-        localField: 'artist',
-        foreignField: '_id',
-        as: 'artist',
-      },
-    },
-    { $unwind: '$artist' },
-    { $match: { ...keyword } },
-    { $match: { ...filter } },
-    {
-      $project: {
-        artCode: 0,
-        theme: 0,
-        inStock: 0,
-        'info.details': 0,
-        'info.createdAt': 0,
-        'artist.code': 0,
-        'artist.record': 0,
-        'artist.thumbnail': 0,
-        'artist.likes': 0,
-        'artist.countOfWorks': 0,
-        'artist.isActive': 0,
-        'artist.joinAt': 0,
-      },
-    },
-    { $sort: { _id: -1 } },
-    { $skip: pageSize * (page - 1) },
-    { $limit: pageSize },
-  ]);
-
-  if (!products.length) {
-    res.json({
-      products,
-      page,
-      pages: 1,
-    });
-    return;
-  }
-
-  count = await Product.aggregate([
-    { $match: { inStock: true } },
-    {
-      $lookup: {
-        from: 'artists',
-        localField: 'artist',
-        foreignField: '_id',
-        as: 'artist',
-      },
-    },
-    { $match: { ...keyword } },
-    { $match: { ...filter } },
-    { $count: 'of_products' },
-  ]);
-
-  res.json({
-    products,
-    page,
-    pages: Math.ceil(count[0].of_products / pageSize),
-  });
-});
-
-// @desc    Update a product
-// @route   PATCH /api/products/:id
-// @access  Private/Admin
-const updateProduct = asyncHandler(async (req, res) => {
-  const updatedProduct = await Product.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-  );
-  res.json(updatedProduct);
-});
-
-// @desc    Delete a product
-// @route   DELETE /api/products/:id
-// @access  Private/Admin
-const deleteProduct = asyncHandler(async (req, res) => {
-  // 재고 있을 시 상품 삭제 가능
-
-  const product = await Product.findOneAndDelete({
-    _id: req.params.id,
-    inStock: true,
+    res.status(HTTP_STATUS.OK).json(data);
   });
 
-  if (product) {
-    res.json({ message: `해당 상품이 삭제되었습니다` });
-  } else {
-    res.status(400).json({ message: '해당 상품은 삭제할 수 없습니다' });
-  }
-});
+  // @desc    Update a product
+  // @route   PATCH /api/products/:id
+  // @access  Private/Admin
+  updateProduct = asyncHandler(async (req, res) => {
+    const data = await this.#productService.updateOne(req.params.id, req.body);
+
+    res.status(HTTP_STATUS.OK).json(data);
+  });
+
+  // @desc    Delete a product
+  // @route   DELETE /api/products/:id
+  // @access  Private/Admin
+  deleteProduct = asyncHandler(async (req, res) => {
+    // 재고 있을 시에만 삭제 가능
+
+    const deleted = await this.#productService.deleteOne(req.params.id);
+
+    const message = deleted
+      ? `해당 상품이 삭제되었습니다`
+      : '해당 상품은 삭제할 수 없습니다';
+
+    res.status(HTTP_STATUS.OK).json({ message });
+  });
+  //-------------
+}
+
+//-----
 
 // @desc   Fetch single product
 // @route  GET /api/products/:id
 // @access Public
-const getProductById = asyncHandler(async (req, res) => {
+export const getProductById = asyncHandler(async (req, res) => {
   const productDetail = await Product.findByIdAndUpdate(
     req.params.id,
     { $inc: { views: 1 } }, // 조회수 올리기!!
@@ -265,7 +112,7 @@ const getProductById = asyncHandler(async (req, res) => {
 // @desc   Fetch latest products
 // @route  GET /api/products/latest
 // @access Public
-const getLatestProducts = asyncHandler(async (req, res) => {
+export const getLatestProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({}, { image: 1 })
     .limit(5)
     .sort({ _id: -1 });
@@ -277,7 +124,7 @@ const getLatestProducts = asyncHandler(async (req, res) => {
 // @desc   Check stock of cartItems
 // @route  GET /api/products/cart-items
 // @access Public
-const getCartItems = asyncHandler(async (req, res) => {
+export const getCartItems = asyncHandler(async (req, res) => {
   // 장바구니 상품 재고 확인 차 요청
   const { productId } = req.query;
 
@@ -299,7 +146,7 @@ const getCartItems = asyncHandler(async (req, res) => {
 // @desc   Fetch cartItems totalprice
 // @route  GET /api/products/total-price
 // @access Private
-const getTotalPrice = asyncHandler(async (req, res) => {
+export const getTotalPrice = asyncHandler(async (req, res) => {
   // 결제로 넘어갈 시 총 상품 금액
   let { productId } = req.query;
 
@@ -324,7 +171,7 @@ const getTotalPrice = asyncHandler(async (req, res) => {
 // @desc   Zzim or unZzim the product
 // @route  PATCH /api/products/zzim
 // @access Private
-const zzimProduct = asyncHandler(async (req, res) => {
+export const zzimProduct = asyncHandler(async (req, res) => {
   // 찜 해체 시에는 id가 배열로 올 수 있다. (선택삭제)
   const { productId, zzim } = req.body;
 
@@ -359,7 +206,7 @@ const zzimProduct = asyncHandler(async (req, res) => {
 // @desc   Fetch products in zzimlist
 // @route  GET /api/products/zzim
 // @access Private
-const getZzimProducts = asyncHandler(async (req, res) => {
+export const getZzimProducts = asyncHandler(async (req, res) => {
   const products = await Product.find(
     { likes: req.user.id },
     {
@@ -376,16 +223,3 @@ const getZzimProducts = asyncHandler(async (req, res) => {
 
   res.json(products);
 });
-
-export {
-  createProduct,
-  updateProduct,
-  deleteProduct,
-  getProducts,
-  getProductById,
-  getLatestProducts,
-  zzimProduct,
-  getZzimProducts,
-  getCartItems,
-  getTotalPrice,
-};
